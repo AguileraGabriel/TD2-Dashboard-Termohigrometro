935bc9d9d784c201b4ff84a468d7cbfd8a8a79d3
Gabriel Aguilera <aguileragabriel98@gmail.com>
2025-01-26 18:34:24 -0300

    Se agrego el grafico

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,31 +29,61 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.txtOutput = new System.Windows.Forms.TextBox();
             this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.chartInyeccionRetorno = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartInyeccionRetorno)).BeginInit();
             this.SuspendLayout();
             // 
             // txtOutput
             // 
-            this.txtOutput.Location = new System.Drawing.Point(73, 54);
+            this.txtOutput.Location = new System.Drawing.Point(75, 371);
             this.txtOutput.Multiline = true;
             this.txtOutput.Name = "txtOutput";
             this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtOutput.Size = new System.Drawing.Size(661, 256);
+            this.txtOutput.Size = new System.Drawing.Size(661, 67);
             this.txtOutput.TabIndex = 0;
             // 
             // serialPort
             // 
             this.serialPort.PortName = "COM3";
             // 
+            // chartInyeccionRetorno
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartInyeccionRetorno.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartInyeccionRetorno.Legends.Add(legend1);
+            this.chartInyeccionRetorno.Location = new System.Drawing.Point(75, 12);
+            this.chartInyeccionRetorno.Name = "chartInyeccionRetorno";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "inyeccion";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "retorno";
+            this.chartInyeccionRetorno.Series.Add(series1);
+            this.chartInyeccionRetorno.Series.Add(series2);
+            this.chartInyeccionRetorno.Size = new System.Drawing.Size(661, 341);
+            this.chartInyeccionRetorno.TabIndex = 1;
+            this.chartInyeccionRetorno.Text = "chart1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chartInyeccionRetorno);
             this.Controls.Add(this.txtOutput);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chartInyeccionRetorno)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -63,6 +93,7 @@
 
         private System.Windows.Forms.TextBox txtOutput;
         private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartInyeccionRetorno;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO.Ports;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+
 
 namespace WindowsFormsApp1
 {
@@ -22,10 +17,15 @@ namespace WindowsFormsApp1
             this.Load += new EventHandler(Form1_Load);
 
         }
+        
         private void Form1_Load(object sender, EventArgs e)
         {
             try
             {
+                // Configurar el gráfico
+                ConfigurarGrafico();
+
+                // Configurar el puerto serie
                 serialPort.DataReceived += SerialPort_DataReceived;
                 serialPort.Open();
             }
@@ -33,6 +33,30 @@ namespace WindowsFormsApp1
             {
                 MessageBox.Show("Error al abrir el puerto: " + ex.Message);
             }
+        }
+
+        private void ConfigurarGrafico()
+        {
+            // Configurar el área del gráfico
+            chartInyeccionRetorno.ChartAreas.Add(new ChartArea("Principal"));
+
+            // Configurar la serie para inyección
+            var serieInyeccion = new Series("Inyección")
+            {
+                ChartType = SeriesChartType.Line,
+                XValueType = ChartValueType.Time,
+                YValueType = ChartValueType.Double
+            };
+            chartInyeccionRetorno.Series.Add(serieInyeccion);
+
+            // Configurar la serie para retorno
+            var serieRetorno = new Series("Retorno")
+            {
+                ChartType = SeriesChartType.Line,
+                XValueType = ChartValueType.Time,
+                YValueType = ChartValueType.Double
+            };
+            chartInyeccionRetorno.Series.Add(serieRetorno);
         }
 
         private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
@@ -58,11 +82,43 @@ namespace WindowsFormsApp1
 
                 // Mostrar el JSON formateado en el TextBox.
                 MostrarDatosEnPantalla(jsonFormateado);
+
+                // Extraer datos para el gráfico
+                double inyeccion = jsonObject["inyeccion"]?.Value<double>() ?? 0;
+                double retorno = jsonObject["retorno"]?.Value<double>() ?? 0;
+                string time = jsonObject["time"]?.Value<string>() ?? DateTime.Now.ToString("HH:mm:ss");
+
+                // Agregar puntos al gráfico
+                AgregarPuntosAlGrafico(time, inyeccion, retorno);
             }
             catch (JsonReaderException)
             {
                 // Si el JSON es inválido, mostrar un mensaje de error.
                 MostrarDatosEnPantalla("Error: El JSON recibido no es válido.");
+            }
+        }
+
+        private void AgregarPuntosAlGrafico(string time, double inyeccion, double retorno)
+        {
+            if (chartInyeccionRetorno.InvokeRequired)
+            {
+                chartInyeccionRetorno.Invoke(new Action(() => AgregarPuntosAlGrafico(time, inyeccion, retorno)));
+            }
+            else
+            {
+                // Agregar puntos a las series
+                chartInyeccionRetorno.Series["Inyección"].Points.AddXY(time, inyeccion);
+                chartInyeccionRetorno.Series["Retorno"].Points.AddXY(time, retorno);
+
+                // Mantener un límite de puntos para evitar saturación
+                if (chartInyeccionRetorno.Series["Inyección"].Points.Count > 100)
+                {
+                    chartInyeccionRetorno.Series["Inyección"].Points.RemoveAt(0);
+                    chartInyeccionRetorno.Series["Retorno"].Points.RemoveAt(0);
+                }
+
+                // Ajustar el eje X automáticamente
+                chartInyeccionRetorno.ChartAreas["Principal"].RecalculateAxesScale();
             }
         }
 
